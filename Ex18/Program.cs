261fa69e6c76d46dea22b1206f38cb8fd26dc33c
Ekaterina <katya.kinal@gmail.com>
2022-08-11 21:39:14 +0300

    Ex18

--- Ex18/Program.cs
+++ Ex18/Program.cs
@@ -1,1 +1,12 @@
 ﻿
+Console.Write("Введите координату Х точки А ");
+int Ax = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Y точки А ");
+int Ay = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Х точки B ");
+int Bx = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Y точки B ");
+int By = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Sqrt(Math.Pow(Ax-Ay,2) + (Math.Pow(Bx-By,2)));
+Console.WriteLine($"Расстояние: {Math.Round(result,3)}");
