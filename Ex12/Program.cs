a3d7490c043af96c8ae9223f0821d1cec83800dd
Ekaterina <katya.kinal@gmail.com>
2022-08-11 21:31:40 +0300

    Ex12

--- Ex12/Program.cs
+++ Ex12/Program.cs
@@ -1,15 +1,14 @@
 ﻿// 17. Напишите программу, которая принимает на вход координаты точки (X и Y), 
 //причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
 
-Console.Write("Введите координату по оси ОХ: ");
-int x = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите координату по оси ОY: ");
-int y = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите номер четверти: ");
+int numberOfQquarter = Convert.ToInt32(Console.ReadLine());
 
-if(x > 0 && y > 0) Console.WriteLine("I четверть");
-if(x < 0 && y > 0) Console.WriteLine("II четверть");
-if(x < 0 && y < 0) Console.WriteLine("III четверть");
-if(x > 0 && y < 0) Console.WriteLine("IV четверть");
+
+if(numberOfQquarter == 1) Console.WriteLine("x > 0 && y > 0");
+if(numberOfQquarter == 2) Console.WriteLine("x < 0 && y > 0");
+if(numberOfQquarter == 3) Console.WriteLine("x < 0 && y < 0");
+if(numberOfQquarter == 4) Console.WriteLine("x > 0 && y < 0");
 
 
 
